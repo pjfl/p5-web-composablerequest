a8461ad7b78af902bb8479339ff462edd17bb88a
pjfl <github@roxsoft.co.uk>
2015-08-09 17:29:16 +0100

    Added Travis suppor

--- lib/Web/ComposableRequest.pm
+++ lib/Web/ComposableRequest.pm
@@ -2,7 +2,7 @@ package Web::ComposableRequest;
 
 use 5.010001;
 use namespace::autoclean;
-use version; our $VERSION = qv( sprintf '0.1.%d', q$Rev: 7 $ =~ /\d+/gmx );
+use version; our $VERSION = qv( sprintf '0.1.%d', q$Rev: 8 $ =~ /\d+/gmx );
 
 use Scalar::Util                      qw( blessed );
 use Web::ComposableRequest::Base;
